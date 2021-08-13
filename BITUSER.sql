d61ff73cefd6b3606fa6351abc3acd428c890955
박상혁 <huhaha9142@naver.com>
2021-08-13 16:22:30 +0900

    Emaillist 파일

---
+++ BITUSER.sql
@@ -0,0 +1,17 @@
+CREATE TABLE emaillist (
+	no NUMBER PRIMARY KEY,
+	last_name VARCHAR2(20) NOT NULL,
+	first_name VARCHAR2(20) NOT NULL,
+	email VARCHAR2(128) NOT NULL,
+	createdAt DATE DEFAULT SYSDATE);
+    
+CREATE SEQUENCE seq_emaillist_pk
+	START WITH 1
+	INCREMENT BY 1;
+    
+INSERT INTO emaillist (no, first_name, last_name, email)
+VALUES(seq_emaillist_pk.NEXTVAL, '상혁', '박', 'huhaha9142@naver.com');
+
+SELECT * FROM emaillist;
+
+COMMIT;
